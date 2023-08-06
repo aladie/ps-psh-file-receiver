bb7336c7d7c8eebaa2ff27f77b44d98b14857858
Lars <47330742+aladie@users.noreply.github.com>
2023-08-06 20:52:39 +0200

    v0.1.2 Fix Typo

--- src/ps-psh-file-receiver-command.py
+++ src/ps-psh-file-receiver-command.py
@@ -4,8 +4,6 @@ import os.path
 import socket
 import re
 
-MAGIC = 0x0000EA6E
-
 def main(args):
     # Validate folder path
     if not os.path.exists(args.folder):
@@ -23,32 +21,94 @@ def main(args):
 
     try:
         # Connect to console
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(3)
-        sock.connect((args.ip, args.port))
+        data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_sock.settimeout(3)
+        data_sock.connect((args.ip, args.port))
 
-        # Receive filename
-        file = args.folder + sock.recv(128).decode('UTF-8')
+        # Receive data_type of send bytes (file/directory/end)
+        data_type = data_sock.recv(1).decode('UTF-8')
 
-        # Download file in chunks
-        with open(file, 'wb') as f:
-            while True:
-                data = sock.recv(4096)
+        current_directory = args.folder
 
-                if data == b'':
-                    f.close()
-                    break
+        # Keep receiving until exit command was received
+        while data_type != 'e':
 
-                f.write(data)
+            if data_type == 'd':
+                # Receive directory name and create new directory
+                temp_dir = data_sock.recv(128).decode('UTF-8')
+                new_directory = current_directory
+
+                for i in range(0, 128):
+                    if temp_dir[i] == '/':
+                        new_directory += '/'
+                        break
+
+                    new_directory += temp_dir[i]
+
+                # Notify console that the new directory was created successfully
+                data_sock.sendall(b'D_OK')
+
+                try:
+                    os.makedirs(new_directory)
+                except FileExistsError:
+                    print("Folder already exists!")
+
+                current_directory = new_directory
+            elif data_type == 'r':
+                # Go up in the directory tree
+                current_directory = current_directory[0:current_directory.rfind('/')]
+                current_directory = current_directory[0:current_directory.rfind('/') + 1]
+
+            elif data_type == 'f':
+                # Receive infos
+                temp_info = data_sock.recv(20).decode('UTF-8')
+                temp_length = ''
+
+                for i in range(0, 20):
+                    if temp_info[i] == 'R':
+                        break
+
+                    temp_length += str(int(temp_info[i]))
+
+                filename_length = int(temp_length)
+
+                # Receive filename
+                filename = data_sock.recv(filename_length).decode('UTF-8')
+
+                file = current_directory + filename
+
+                # Notify console of successfull filename transfer
+                data_sock.sendall(b'F_OK')
+
+                # Download file in chunks
+                with open(file, 'wb') as f:
+                    while True:
+                        # VERY EXPERIMENTAL WAY OF CLOSING THE FILE!!
+                        try:
+                            data = data_sock.recv(4096)
+                        except socket.timeout:
+                            f.close()
+                            break
+
+                        f.write(data)
+
+                # Notify console of possible successfull file transfer
+                data_sock.sendall(b'R_OK')
+            else:
+                print("Something broke while communicating with the console")
+
+            # Receive data_type of send bytes (file/directory/end)
+            data_type = data_sock.recv(1).decode('UTF-8')
 
         # Close connection
-        sock.close()
+        data_sock.close()
+
     except socket.error:
         print('Failed to connect to ' + args.ip + ' on port ' + str(args.port) + '!')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Receive a file over TCP.')
     parser.add_argument('-i', '--ip', required=True, help='The server IP address.')
-    parser.add_argument('-p', '--port', type=int, default=9045, help='The server port number.')
+    parser.add_argument('-p', '--port', type=int, default=9046, help='The server port number.')
     parser.add_argument('-f', '--folder', required=True, help='The target folder.')
     main(parser.parse_args())
